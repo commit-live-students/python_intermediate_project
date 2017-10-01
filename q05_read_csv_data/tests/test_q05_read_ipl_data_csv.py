a315a3a2f789c36384019d8bf8341eedba613b26
adimyth <mishraaditya6991@gmail.com>
2017-10-01 17:50:10 +0530

    Update

--- q05_read_csv_data/tests/test_q05_read_ipl_data_csv.py
+++ q05_read_csv_data/tests/test_q05_read_ipl_data_csv.py
@@ -8,6 +8,6 @@ from q05_read_csv_data.build import read_ipl_data_csv
 
 class TestRead_ipl_data(TestCase):
     def test_read_ipl_data(self):
-        ipl_matches_array = read_ipl_data_csv('../../data/ipl_matches_small.csv', dtype='|S50')
+        ipl_matches_array = read_ipl_data_csv('data/ipl_matches_small.csv', dtype='|S50')
         self.assertTrue(ipl_matches_array.shape == (1451, 23))
         self.assertTrue(ipl_matches_array.dtype == '|S50')
