99703daf8de018c4b62fc454bae34dc9802d87a8
greyatomtest <greyatomtest@gmail.com>
2017-10-01 07:40:10 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -4,6 +4,8 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir), '..','..'))
 import numpy as np
 
-# Write your code here
-
-
+# Write your code; here
+def create_3d_array():
+    ls = np.arange(0, 27, 1)
+    arr = ls.reshape(3, 3, 3)
+    return arr
