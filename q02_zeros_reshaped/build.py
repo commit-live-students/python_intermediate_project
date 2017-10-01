dd0d4e847723223962d871d2d4fa2077bb300bf9
greyatomtesting <neelkanth.mehta@rediffmail.com>
2017-10-01 09:16:26 +0000

    Done

--- q02_zeros_reshaped/build.py
+++ q02_zeros_reshaped/build.py
@@ -3,6 +3,8 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
 import numpy as np
-from q01_zeros_array.build import zeros_array
+from q01_zeros_array.build import array_zeros
 
-# Write your code
+def array_reshaped_zeros():
+    zeros_array_reshaped = array_zeros().reshape(2,3,4)
+    return zeros_array_reshaped
