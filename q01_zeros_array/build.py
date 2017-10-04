ff1b55dc403b30dd3fbce43b443a087c0b86a24d
arushikumar <arushikumar14@gmail.com>
2017-10-04 17:30:55 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -4,5 +4,8 @@ sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
 # Your solution
-
-
+def func() :
+    arr = [[[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0]]]
+    return np.array(arr)
+array_zeros = func()
+print array_zeros
