c101a26f89b1fbde1a8383b1f91c8c03ba24c9f5
arushikumar <arushikumar14@gmail.com>
2017-10-04 18:30:53 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -4,8 +4,16 @@ sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
 # Your solution
-def func() :
-    arr = [[[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0]]]
-    return np.array(arr)
-array_zeros = func()
-print array_zeros
+#def func() :
+#    arr = [[[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0]]]
+#    return np.array(arr)
+#array_zeros = func()
+#print array_zeros
+
+lst = [0] * 24
+
+def array_zeros() :
+    arr = np.array(lst)
+    arr2 =arr.reshape(3,4,2)
+    return arr2
+array_zeros()
--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -2,3 +2,10 @@
 import numpy as np
 
 # Enter code here
+
+def read_ipl_data_csv(path, **kwargs) :
+    data = np.genfromtxt(path, dtype = "|S50", skip_header = 0, delimiter = ",")
+    ipl_matches_array = np.array(data)
+    return ipl_matches_array
+
+read_ipl_data_csv("./data/ipl_matches_small.csv")
