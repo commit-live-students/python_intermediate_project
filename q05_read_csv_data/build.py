57b21965ebd2635f87dd83f291b840c40c13296d
sudharsanasai <Sudharsanasai@gmail.com>
2017-10-04 14:26:52 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,21 @@
 # Default imports
 import numpy as np
+import csv
+path = "./data/ipl_matches_small.csv"
 
+
+def read_csv_data_to_ndarray(path,dtype=np.float64):
+
+    with open(path,'r') as line:
+        read_line = csv.reader(line)
+        c=0
+        array = np.array([1])
+        for element in read_line:
+            if c ==0:
+                c=1
+                pass
+            else:
+                #print(element)
+                array = np.append(array,element)
+    return array[1:]
 # Enter code here
