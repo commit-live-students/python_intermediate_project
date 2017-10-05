6aef8b61a4f2ffbe38827cae05a29172a7630bde
rdubewar <rdubewar@gmail.com>
2017-10-05 04:04:55 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -3,6 +3,10 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
-# Your solution
+#zeros_array = [[0,0,0],[0,0,0,0],[0,0]]
 
-
+def array_zeros():
+    global zeros_array
+    zeros_array = np.zeros((3, 4, 2))
+#    zeros_array = np.array(zeros_array)
+    return zeros_array
