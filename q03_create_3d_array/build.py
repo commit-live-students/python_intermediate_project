82b9bade4246a02e1f95c485a65375c4f2ea5efe
sharax <sharathdharmaji@gmail.com>
2017-10-05 07:08:29 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,8 @@
-# Default Imports
 import numpy as np
 
-# Enter solution here
+def create_3d_array():
+    n=3;
+    elecount=27
+
+    arr=np.array([i for i in range(0,27)])
+    return arr.reshape((3,3,3))
