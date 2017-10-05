e26678ea2777289c79faeb41c183efc3aa607f1d
apoorvabanubakode <apoorva.banubakode@gmail.com>
2017-10-05 04:02:13 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,14 @@
-# Default Imports
 import numpy as np
 
 # Enter solution here
+
+def create_3d_array():
+    oned=np.arange(0,27)
+    numpyarray=oned.reshape((3,3,3))
+
+#    print(numpyarray)
+
+
+    return numpyarray
+#numpyarray=create_3d_array()
+#print(numpyarray)
