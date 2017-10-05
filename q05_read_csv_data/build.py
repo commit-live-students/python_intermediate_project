15f84a460481c5d1d693de15a0a8b43c6f57ad3e
apoorvabanubakode <apoorva.banubakode@gmail.com>
2017-10-05 07:27:24 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,11 +1,39 @@
-# %load q05_read_csv_data/build.py
-# Default imports
-import numpy as np
-path = "./data/ipl_matches_small.csv"
-# Enter code here
-def read_ipl_data_csv(path,dtype='|S50'):
-    ipl_matches_array=np.genfromtxt(path,dtype='|S50',delimiter=",",skip_header=1)
+# %load q05_runs/build.py
+# Default Imports
+from greyatomlib.python_getting_started.q01_read_data.build import read_data
+from pprint import pprint
+data = read_data()
+#pprint(data['innings'][0]['1st innings']['deliveries'])
+'''run=0
+lst_of_balls=data['innings'][0]['1st innings']['deliveries']
+for balls in lst_of_balls:
+    value_per_ball=balls.values()
+    batsman=value_per_ball[0]['batsman']
+    if batsman=="BB McCullum":
+        print("BB McCullum")
+        run=run+value_per_ball[0]['runs']['batsman']
+print(run)
 
-    return ipl_matches_array
-#ipl=read_ipl_data_csv(path)
-#print(ipl)
+'''
+
+
+
+# Your Solution
+def BC_runs(data):
+    runs=0
+    lst_of_balls=data['innings'][0]['1st innings']['deliveries']
+    for balls in lst_of_balls:
+        value_per_ball=balls.values()
+        batsman=value_per_ball[0]['batsman']
+        if batsman=="BB McCullum":
+            #print("BB McCullum")
+            runs=runs+value_per_ball[0]['runs']['batsman']
+    #print(run)
+
+
+    # Write your code here
+
+
+    return(runs)
+#run=BC_runs(data)
+#print(run)
--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -1,5 +1,25 @@
+# %load q06_get_unique_matches_count/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 path = 'data/ipl_matches_small.csv'
+
+from pprint import pprint
+def get_unique_matches_count():
+    ipl_matches_array=read_ipl_data_csv(path,'|S50')
+
+
+    all1=ipl_matches_array[:,0]
+
+    newlst=[]
+    for ids in all1:
+        if ids not in newlst:
+            newlst.append(ids)
+
+
+    cnt=0
+    for item in newlst:
+        cnt=cnt+1
+    return cnt
+
 
 # Enter Code Here
