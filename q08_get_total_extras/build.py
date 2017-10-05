6a2aa45fe3c4382464e0a507ccc3fb241453000f
shashi07 <shashi.mishra43@gmail.com>
2017-10-05 04:03:10 +0000

    Done

--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -5,3 +5,8 @@ import numpy as np
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+def get_total_extras():
+    data = read_ipl_data_csv(path, dtype=np.int64)
+    data = data[:,17]
+    s = np.sum(data)
+    return s
