645f526423869bf95b9e57e9a25dd505aa011dcd
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-10-07 13:56:08 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -6,4 +6,4 @@ def read_ipl_data_csv(path,dtype='|S50'):
     ipl_matches_array = np.genfromtxt(path,dtype=dtype,delimiter=',',skip_header=1)
     return ipl_matches_array
 
-print read_ipl_data_csv('data/ipl_matches_small.csv',dtype='|S50')
+print read_ipl_data_csv('data/ipl_matches_small.csv','|S50')
