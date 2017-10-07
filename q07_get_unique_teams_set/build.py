ddc4c700d5a4ea487b031d89f3086e302f4bfc9f
sandiprb <baradiya.sandip@gmail.com>
2017-10-07 12:48:14 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -3,3 +3,11 @@ from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_dat
 path = "data/ipl_matches_small.csv"
 
 # Enter Code Here
+def get_unique_teams_set():
+    teams = []
+    for i in read_ipl_data_csv(path, dtype='|S100'):
+        if not i[3] in teams:
+            teams.append(i[3])
+        if not i[4] in teams:
+            teams.append(i[4])
+    return set(teams)
