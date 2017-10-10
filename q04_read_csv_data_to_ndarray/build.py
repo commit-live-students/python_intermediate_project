e701565243464646a7f05ee59fa7b2f56aa56567
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-10-10 06:16:08 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -2,4 +2,12 @@
 import numpy as np
 path = "./data/ipl_matches_small.csv"
 
-# Enter code here
+def read_csv_data_to_ndarray (path,dtype=np.float64):
+    path1 = path
+    dtype1 = dtype
+    ipl = np.genfromtxt(path1,dtype=dtype1, skip_header=1,delimiter = ',')
+    return ipl
+
+
+
+read_csv_data_to_ndarray(path ,'|S50')
--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,10 @@
 # Default imports
 import numpy as np
 
+def read_ipl_data_csv():
+    ipl_matches_array = np.genfromtxt("./data/ipl_matches_small.csv", dtype='|S50', delimiter=",")
+    return ipl_matches_array
+
+read_ipl_data_csv()
+
 # Enter code here
