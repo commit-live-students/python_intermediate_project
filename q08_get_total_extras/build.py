dd733de2fec0e8412c7c2c614c4d2604a230b4e3
sagard21 <sagard21@gmail.com>
2017-10-21 21:55:24 +0000

    Done

--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -5,3 +5,7 @@ import numpy as np
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+def get_total_extras():
+    arr = np.genfromtxt("data/ipl_matches_small.csv", delimiter = ',', dtype = '|S50', skip_header = 1)
+    extras = arr[:,17:18].astype(np.int32)
+    return np.sum(extras)
