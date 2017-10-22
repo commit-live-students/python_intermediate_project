3e33235a7e3671826f49a2ce3fef96149f557000
nikhil-ds <prasadnikhil25@gmail.com>
2017-10-22 06:14:45 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -3,11 +3,10 @@ import numpy as np
 
 # Enter solution here
 def create_3d_array():
+    #initialize an empty array of (3,3,3) dimension
     numpy_3d_array = np.empty((3,3,3))
-    #print(numpy_3d_array)
+    #create an array using np.arange of size N i.e size of (3,3,3) np array
     temp_array = np.arange(0, (numpy_3d_array.size))
+    #reshape the array using .reshape
     numpy_3d_array = temp_array.reshape((3,3,3))
     return numpy_3d_array
-    #print(temp_array)
-    #print(numpy_3d_array)
-#create_3d_array()
--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -3,3 +3,8 @@ import numpy as np
 path = "./data/ipl_matches_small.csv"
 
 # Enter code here
+def read_csv_data_to_ndarray(path, data_type=np.float64):
+    data = np.genfromtxt(path, dtype = data_type, delimiter = ',', skip_header = 1)
+    #print(data)
+    return data
+#read_csv_data_to_ndarray(path = './data/ipl_matches_small.csv')
