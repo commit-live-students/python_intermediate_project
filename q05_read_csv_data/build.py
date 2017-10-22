a5d0ba9d59c0bd9cb576b53d138ebc7e6ef22012
nikhil-ds <prasadnikhil25@gmail.com>
2017-10-22 08:37:14 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -2,3 +2,9 @@
 import numpy as np
 
 # Enter code here
+def read_ipl_data_csv(path, dtype='|S50'):
+    dtype = '|S50'
+    ipl_matches_array = np.genfromtxt(path, dtype = dtype, delimiter = ',', skip_header = 1)
+    #print(ipl_matches_array)
+    return ipl_matches_array
+#read_ipl_data_csv(path = './data/ipl_matches_small.csv',dtype = 'float64')
