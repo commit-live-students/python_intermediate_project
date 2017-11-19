569e4027b36cd49ea88adae48f54df37dba50def
chiragpujari <chg.p786@gmail.com>
2017-11-19 08:30:50 +0000

    Done

--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,7 +1,20 @@
 # Default Imports
-from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
-
-path = 'data/ipl_matches_small.csv'
+from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
+path = "data/ipl_matches_small.csv"
 
 # Enter Code Here
+def get_unique_teams_set():
+    count=0
+    teams=set()
+    data=read_ipl_data_csv(path,"|S50")
+    for i in range(0,data.shape[0]):
+        if data[i][3] != data[i-1][3]:
+            if data[i][3] not in teams:
+                teams.add(data[i][3])
+        if data[i][4] != data[i-1][4]:
+            if data[i][4] not in teams:
+                teams.add(data[i][4])
+    count=len(teams)
+
+    return count
