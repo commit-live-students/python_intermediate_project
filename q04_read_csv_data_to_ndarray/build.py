267ba805e9f0f23537af7b6e4f27ecc41dc162a6
ms10031990 <ms10031990@yahoo.com>
2017-11-29 12:49:38 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,9 +1,10 @@
 # Default Imports
 import numpy as np
 path = "./data/ipl_matches_small.csv"
+dtype = "|S50"
 
-def read_csv_data_to_ndarray(path, in_dtype):
+def read_csv_data_to_ndarray(path, dtype):
     with open(path, 'r') as stream:
-        out_array = np.genfromtxt(stream, skip_header=1, delimiter=',', dtype=in_dtype)
+        out_array = np.genfromtxt(stream, skip_header=1, delimiter=',', dtype=dtype)
 
     return out_array
--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,9 @@
-# Default imports
 import numpy as np
+from q04_read_csv_data_to_ndarray.build import read_csv_data_to_ndarray
 
-# Enter code here
+path = "./data/ipl_matches_small.csv"
+
+def read_ipl_data_csv(path, dtype = "|S50"):
+
+    ipl_matches_array = read_csv_data_to_ndarray(path, dtype)
+    return ipl_matches_array
