fbd5ccb187dfa777576d5fc88a57c9f453800c8e
swapnilpatil22 <swapnil.patil2205@hotmail.com>
2017-12-02 13:39:27 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -2,3 +2,6 @@
 import numpy as np
 
 # Enter code here
+def read_ipl_data_csv(path,dtype=None):
+    ipl_matches_array=np.genfromtxt(path,delimiter=',',dtype=dtype,skip_header=True)
+    return ipl_matches_array
