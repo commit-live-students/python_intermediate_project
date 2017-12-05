17db1953cc4c90477a0e2e279cd7497ef1297670
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2017-12-05 05:42:43 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,12 @@
 # Default Imports
 import numpy as np
+import numpy.random as rd
 
 # Enter solution here
+def create_3d_array():
+    N = 3*3*3
+    array3d = np.arange(0,N)
+    reshaped = array3d.reshape(3,3,3)
+    return reshaped
+
+print create_3d_array()
