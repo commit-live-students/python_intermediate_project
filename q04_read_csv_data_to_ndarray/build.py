fbda1d91e3f2b354a6e4bcb9fa20d2abd2026cc9
abkedar <anupbhande@gmail.com>
2017-12-07 07:31:43 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,12 +1,10 @@
 import numpy as np
+
 path = "./data/ipl_matches_small.csv"
 dttype = 'str'
 
+def read_csv_data_to_ndarray(path_par, dttype):
+    data = np.loadtxt(path_par, delimiter=",", skiprows=1,dtype=dttype)
+    return data
 
-def read_csv_data_to_ndarray(pathr, dttpye):
-    read1 = np.loadtxt(pathr, delimiter = ',', skiprows= 1, dtype = dttype)
-
-
-    #print type(read1)
-    return read1
 print read_csv_data_to_ndarray(path, dttype)
