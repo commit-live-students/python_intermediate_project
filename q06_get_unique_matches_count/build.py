955bbf177e040f486210395bc002f058482bb35f
abkedar <anupbhande@gmail.com>
2017-12-07 08:08:25 +0000

    Done

--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -2,4 +2,14 @@
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 path = 'data/ipl_matches_small.csv'
 
+import numpy as np
 # Enter Code Here
+def get_unique_matches_count():
+    #ipl_matches_arraay = np.loadtxt('data/ipl_matches_small.csv')
+    data1 =  np.genfromtxt(path, dtype = 'str', delimiter = ",", skip_header = 1)
+    data1 = data1[:, :1]
+    ipl_matches_array = np.unique(data1).shape[0]
+    #print type(ipl_matches_array)
+    return ipl_matches_array
+
+print get_unique_matches_count()
--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,14 @@
-# Default imports
+import numpy as np
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 path = "data/ipl_matches_small.csv"
 
-# Enter Code Here
+
+def get_unique_teams_set():
+    teams = np.genfromtxt(path, dtype = '|S50', delimiter = ",", skip_header = 1, usecols = (3, 4))
+    team1 = (teams[:, :1])
+    team2 = (teams[:, 1:2])
+    total_team = np.union1d(team1, team2)
+    #total_team = np.hstack((team1, team2))
+    total_team = set(total_team)
+    return (total_team)
+print get_unique_teams_set()
