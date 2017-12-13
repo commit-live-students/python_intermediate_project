b587367bd2ebb3bb63be8f35dceb6f0eb14439d3
oldflame <dhavl32@gmail.com>
2017-12-13 15:17:27 +0000

    Done

--- q02_zeros_reshaped/build.py
+++ q02_zeros_reshaped/build.py
@@ -3,3 +3,6 @@ import numpy as np
 from greyatomlib.python_intermediate.q01_zeros_array.build import  array_zeros
 
 # Write your code
+def array_reshaped_zeros():
+    zeros = np.zeros((2,3,4))
+    return zeros
