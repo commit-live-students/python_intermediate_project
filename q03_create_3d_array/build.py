3bab50443f57547df6bce4b9682f2abda89c5a8d
nazirwa <nazirmail@gmail.com>
2017-12-13 18:37:29 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -2,3 +2,10 @@
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    N = 27
+    nparray = np.arange(start=0, stop=27, dtype=np.int).reshape(3,3,3)
+    #nparray = np.zeros((0,N-1))
+    return nparray
+
+print create_3d_array()
