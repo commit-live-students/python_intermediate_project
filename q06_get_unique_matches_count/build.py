a326b2a601e232631d47c589f2cee1046d852ed0
harshavachhani <harshavachhani@gmail.com>
2017-12-15 17:09:11 +0000

    Done

--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -3,3 +3,10 @@ from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_dat
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+def get_unique_matches_count():
+    count=0
+    data=read_ipl_data_csv(path,"|S50")
+    for i in range(0,data.shape[0]):
+        if data[i][2] != data[i-1][2]:
+            count+=1
+    return count
