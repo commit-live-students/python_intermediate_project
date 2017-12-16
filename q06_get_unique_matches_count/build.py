ce087b5a880a3455e06a5d80471cd177854b22f2
oldflame <dhavl32@gmail.com>
2017-12-16 11:22:40 +0000

    Done

--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -1,5 +1,10 @@
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 path = 'data/ipl_matches_small.csv'
-
+import pandas as pd
 # Enter Code Here
+def get_unique_matches_count():
+    ipl_matches_array = pd.read_csv(path)
+    count = ipl_matches_array.groupby('match_code').size()
+    ipl_matches_array = count.count()
+    return ipl_matches_array
--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,11 @@
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
+import pandas as pd
 path = "data/ipl_matches_small.csv"
 
 # Enter Code Here
+def get_unique_matches_count(path):
+    ipl_matches_array = pd.read_csv(path)
+    count = ipl_matches_array.groupby('match_code').size()
+    ipl_matches_array = count.count()
+    return ipl_matches_array
