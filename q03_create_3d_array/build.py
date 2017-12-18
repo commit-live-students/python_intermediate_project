bc4db8cad338c132154950fae7b7cd1b40facdf6
Dhivya-sekar <dhivya.SK@gmail.com>
2017-12-18 18:12:39 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,9 @@
-# Default Imports
 import numpy as np
-
-# Enter solution here
+def create_3d_array():
+    a1 = np.arange(27)
+    a2 = a1.size
+    a3 = a1.reshape(3,3,3)
+    #print a1
+    #print a3
+    #print type(a3)
+    return a3
