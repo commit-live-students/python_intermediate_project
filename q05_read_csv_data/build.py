8f8bca0ff2f62e6fcf64570ccd5f77467bf9a837
yoganct <yoganct@gmail.com>
2017-12-19 17:03:06 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,5 @@
 # Default imports
 import numpy as np
-
-# Enter code here
+def read_ipl_data_csv(path, dtype='|S50'):
+    numpy_array = np.genfromtxt(path,delimiter=',',dtype='|S50',skip_header=1)
+    return numpy_array
