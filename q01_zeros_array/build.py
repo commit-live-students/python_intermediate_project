86e340ea5c9a84b1eb8cf863a5bcbb3a209bb2a7
aarthikat <aarthika.t@gmail.com>
2017-12-19 17:44:24 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -3,6 +3,9 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
+def array_zeros():
+    arr=np.zeros((3,4,2))
+    return arr
+
+array_zeros()
 # Your solution
-
-
