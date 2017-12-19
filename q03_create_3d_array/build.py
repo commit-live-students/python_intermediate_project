d9a46b66d2d983f1443a9dc57dc504086266e014
Mayank211 <mayank2saxena@gmail.com>
2017-12-19 16:10:05 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -2,3 +2,9 @@
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    N=3*3*3
+    array_a = np.array((range(N)))
+    array_a = array_a.reshape((3,3,3))
+    return array_a
+print create_3d_array()
