2396a462ab91059aeedc6b7febd65dc5cc984d76
Vijay1306 <vijaymadurai@yahoo.com>
2017-12-24 16:29:06 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -3,3 +3,7 @@ import numpy as np
 path = "./data/ipl_matches_small.csv"
 
 # Enter code here
+
+def read_csv_data_to_ndarray(path, input_dtype):
+    iplarray = np.genfromtxt(path,input_dtype, delimiter=',', skip_header = 1)
+    return iplarray
