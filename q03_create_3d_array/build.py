aafdb53fea5ea4e66f80d4e062c9601c14288334
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-12-31 07:42:38 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -2,3 +2,7 @@
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    dim = (3,3,3)
+    nDarr = np.asarray([i for i in range(np.size(np.ndarray(dim)))])
+    return np.reshape(nDarr,dim)
