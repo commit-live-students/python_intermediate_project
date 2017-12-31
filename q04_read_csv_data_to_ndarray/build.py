96dc049e41afe778006158a95693e717695c8082
aarthikat <aarthika.t@gmail.com>
2017-12-31 10:45:36 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -2,4 +2,9 @@
 import numpy as np
 path = "./data/ipl_matches_small.csv"
 
+def read_csv_data_to_ndarray(path,dtype):
+    second =np.genfromtxt(path,delimiter=',',dtype=dtype,skip_header=1)
+    return second
+
+read_csv_data_to_ndarray(path,np.float64)
 # Enter code here
--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -1,5 +1,24 @@
 # Default imports
+import numpy as np
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+def get_unique_matches_count():
+    ipl_matches_array=read_ipl_data_csv(path,np.float64)
+    #unique_elements,count_elements= np.unique(ipl_matches_array,return_counts=True)
+    #np.count_nonzero(ipl_matches_array=='match_code')
+    #unique_elements,count_elements== np.unique(ipl_matches_array,return_counts=True)
+    #np.count_nonzero(unique_elements=='match_code')
+    #freq=np.bincount(count_elements)
+    #print freq
+    #print unique_elements,count_elements
+    #print unique_elements[0]
+    #print count_elements[3]
+
+    unique_matches_count=len(np.unique(ipl_matches_array[0:,0]))
+    #print np.unique(ipl_matches_array[0:,0])
+    return unique_matches_count
+
+    #print np.array([unique_elements,freq]).T
+get_unique_matches_count()
