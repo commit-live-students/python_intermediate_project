22aabad7418545f21fb70a9ba0b87026476a6760
siddhichandan <siddhichandan@gmail.com>
2018-01-22 17:41:47 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -3,5 +3,5 @@ import numpy as np
 
 # Enter code here
 def read_ipl_data_csv(path, dtype='|S50'):
-    csv = np.genfromtxt(path,delimiter=",",skip_header=1,dtype=dtype)
-    return csv
+    ipl_matches_array = np.genfromtxt(path,delimiter=",",skip_header=1,dtype=dtype)
+    return ipl_matches_array
