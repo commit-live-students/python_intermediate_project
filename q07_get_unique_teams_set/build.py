c654f30e6e85d4328be576aa3c087a318f74d6ae
yuvrajkale87 <yuvraj.kale87@gmail.com>
2018-01-26 13:42:50 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -3,3 +3,18 @@ from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_dat
 path = "data/ipl_matches_small.csv"
 
 # Enter Code Here
+
+import numpy as np
+import pandas as pd
+
+# Enter Code Here
+def get_unique_teams_set():
+    ipl_matches_array = read_ipl_data_csv(path,dtype = '|S50')
+    b = np.array([])
+
+    for i in ipl_matches_array:
+        b = np.append(b,i[3])
+        b = np.append(b,i[4])
+
+        count = set(b)
+    return count
