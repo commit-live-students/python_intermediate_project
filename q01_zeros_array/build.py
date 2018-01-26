999a1aae4d362788e40562e30f9020881b81f267
kvin15 <vinayakkhamkar@gmail.com>
2018-01-26 19:09:55 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -3,6 +3,7 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
-# Your solution
-
-
+# return array of zeros with dimension as 3, 4, 2
+def array_zeros():
+    zeros_array = np.zeros((3,4,2),dtype=np.int8)
+    return zeros_array
