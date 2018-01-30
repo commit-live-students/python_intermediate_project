ffd1f0cf1b82aeed9e9e610e71123d042953eda2
TheSarang <narkhedesarang@gmail.com>
2018-01-30 12:54:29 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,8 +1,28 @@
+# %load q01_zeros_array/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
 # Your solution
+def array_zeros():
+    zeros_array = (
+        ((1,2),
+         (3,4),
+         (5,6),
+         (7,8)),
+        ((11,12),
+         (13,14),
+         (15,16),
+         (17,18)),
+        ((11,12),
+         (13,14),
+         (15,16),
+         (17,18)),
+    )
+    d = np.array(zeros_array)
+    a = np.zeros((3,4,2))
+    zeros_array = a
+    return zeros_array
 
 
