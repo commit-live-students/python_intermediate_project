5c01537689005239bfbb0e4de26e1466ba256cba
TheSarang <narkhedesarang@gmail.com>
2018-01-31 07:34:18 +0000

    Done

--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -5,20 +5,11 @@ from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_dat
 path = 'data/ipl_matches_small.csv'
 
 def get_unique_matches_count():
-    ipl_matches_array = np.genfromtxt(path, dtype=None, delimiter=',', skip_header=1,usecols=(0))
-    print ipl_matches_array
-    ipl_matches_array = np.unique(ipl_matches_array,return_counts=True)
-    print ipl_matches_array
-    match_code, counts = ipl_matches_array
-    zipping = zip(match_code, counts)
-    ipl_matches_array = 6
-    return ipl_matches_array
-    #print counts[0] + counts[1] + counts[2] + counts[3] + counts[4] + counts[5]
-    '''i=0
-    b=0
-    while i<len(counts):
-        #print counts[i]
-        b += counts[i]
-        print b
-        i+=1'''
+    ipl_matches_array = np.genfromtxt(path, dtype=None, delimiter=',', skip_header=1,usecols=(0)) #read csv file
+    ipl_matches_array = np.unique(ipl_matches_array,return_counts=True) #find unique values
+    match_code, counts = ipl_matches_array #saperate unique values and their counts
+    #zipping  = zip(match_code, counts) #zip those together. It is an unnecessary step.
+    zipping = np.array(counts)
+    return zipping.size
+
 
