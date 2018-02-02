5db88e0c5b879781a72028a3e0a5a79f08d8ca3b
CharulataShelar <charulatashelar@gmail.com>
2018-02-02 10:15:55 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -5,4 +5,11 @@ import numpy as np
 
 # Your solution
 
+def array_zeros():
+    zeros_array = np.zeros((3,4,2))#12).reshape(3,4)
+    #print zeros_array
+    #print zeros_array.shape
+    #print type(zeros_array)
+    return zeros_array
 
+#array_zeros()
