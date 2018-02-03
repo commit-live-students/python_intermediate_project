585d1f4f72d09c337a85eb98eb32312b48812f39
sacvijai <sacvijai@gmail.com>
2018-02-03 08:22:41 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -4,5 +4,20 @@ sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
 # Your solution
-
-
+def array_zeros():
+    pcm = [
+        [[0,0],
+         [0,0],
+         [0,0],
+         [0,0]],
+        [[0,0],
+         [0,0],
+         [0,0],
+         [0,0]],
+        [[0,0],
+         [0,0],
+         [0,0],
+         [0,0]]
+]
+    zeros_array = np.array(pcm)
+    return zeros_array
--- q02_zeros_reshaped/build.py
+++ q02_zeros_reshaped/build.py
@@ -3,3 +3,20 @@ import numpy as np
 from greyatomlib.python_intermediate.q01_zeros_array.build import  array_zeros
 
 # Write your code
+def array_zeros(self):
+       self.pcm = [
+        [[0,0],
+         [0,0],
+         [0,0],
+         [0,0]],
+        [[0,0],
+         [0,0],
+         [0,0],
+         [0,0]],
+        [[0,0],
+         [0,0],
+         [0,0],
+         [0,0]]
+]
+    zeros_array = np.array(pcm)
+return zeros_array
