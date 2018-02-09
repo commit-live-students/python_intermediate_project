6b114177410ce5ce292c97f171cc368b9ccda019
Ketan10994 <katcee10@gmail.com>
2018-02-09 11:46:44 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,14 @@
 # Default Imports
 import numpy as np
 
-# Enter solution here
+def create_3d_array():
+    a = np.zeros(shape=(3,3,3))
+    #print a
+    array = np.size(a)
+    print array
+    array_number =  np.arange(0,27)
+    print array_number
+    variable = np.reshape(array_number,newshape = (3,3,3))
+    return variable
+
+print create_3d_array()
