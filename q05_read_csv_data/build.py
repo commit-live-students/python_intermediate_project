3bf5554824295dd6c94d4dfa5803577c0b005edb
sonal7930 <sonal.chandra7930@gmail.com>
2018-02-14 14:05:32 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -4,4 +4,5 @@ from io import BytesIO
 path = "./data/ipl_matches_small.csv"
 
 def read_ipl_data_csv():
-    
+    ipl_matches_array=np.genfromtxt(BytesIO(path),skip_header=1,delimiter=",",dtype="|S50")
+    return ipl_matches_array
