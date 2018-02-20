46f0ffa3855032ea0d964c520e8f8903d218bc16
sonal7930 <sonal.chandra7930@gmail.com>
2018-02-20 13:29:01 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -3,10 +3,7 @@
 import numpy as np
 
 path = './data/ipl_matches_small.csv'
+dtype='|S50'
 
-
-
-def read_ipl_data_csv(path):
-    return np.genfromtxt(path,skip_header=1,delimiter=',')
-
-print read_ipl_data_csv(path)
+def read_ipl_data_csv(path,dtype):
+    return np.genfromtxt(path,dtype,skip_header=1,delimiter=',')
