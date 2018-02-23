4c008b93f8c64d5c142942287b41e51f736520ca
mancbg <manasigund22@gmail.com>
2018-02-23 08:52:41 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -2,3 +2,7 @@
 import numpy as np
 
 # Enter code here
+dtype='|S20'
+def read_ipl_data_csv(path, dtype):
+    ipl_matches_array = np.genfromtxt(path, dtype=dtype, skip_header=1, delimiter=",")
+    return ipl_matches_array
