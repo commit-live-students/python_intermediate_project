b59c6f0f48e5ca452aa521d973da620861019f31
mihirpanchal4 <mihirpanchal4@yahoo.com>
2018-03-08 16:19:25 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -4,5 +4,8 @@ sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
 # Your solution
+def array_zeros():
+    n1 = np.zeros((3,4,2))
+    return n1
 
-
+print array_zeros()
--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -2,3 +2,8 @@
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    n1 = np.arange(27).reshape(3,3,3)
+    return create_3d_array
+
+print create_3d_array()
