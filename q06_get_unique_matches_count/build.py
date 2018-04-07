92ee2864506674915a0a97d254c1f7822a4e813f
rajeshu80 <rajesh.u80@gmail.com>
2018-04-07 13:02:12 +0000

    Done

--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -1,5 +1,12 @@
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 path = 'data/ipl_matches_small.csv'
-
+import numpy as np
 # Enter Code Here
+dtype='|S20'
+def get_unique_matches_count():
+     data=np.genfromtxt("data/ipl_matches_small.csv", skip_header=1,delimiter=",", dtype=np.int64);
+     match_code = data[:,0]
+     bincount = np.bincount(match_code)
+     ipl_matches_array = np.count_nonzero(bincount)
+     return ipl_matches_array
