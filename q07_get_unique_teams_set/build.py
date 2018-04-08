02e8a9d92a18ffdb3f6c8d670bbdf071a520f134
rajeshu80 <rajesh.u80@gmail.com>
2018-04-08 09:26:20 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,12 @@
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 path = "data/ipl_matches_small.csv"
-
+import numpy as np
 # Enter Code Here
+dtype='|S50'
+def get_unique_teams_set():
+    data=read_ipl_data_csv(path,dtype)
+    team1= data[:,3].astype(np.str)
+    team2= data[:,4].astype(np.str)
+    variable=np.union1d(team1,team2)
+    return np.all(variable)
--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -3,5 +3,10 @@ from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_dat
 import numpy as np
 
 path = 'data/ipl_matches_small.csv'
-
+dtype='|S20'
 # Enter Code Here
+def get_total_extras():
+    data=np.genfromtxt(path, skip_header=1,delimiter=",",dtype=dtype);
+    x=data[:,17].astype(np.int)
+    extrun = np.sum(x)
+    return extrun
