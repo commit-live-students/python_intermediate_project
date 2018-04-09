d88af8d14baacc6e7f3f8242f84dc389e47e86a7
TheSarang <narkhedesarang@gmail.com>
2018-04-09 06:37:31 +0000

    Done

--- q02_zeros_reshaped/build.py
+++ q02_zeros_reshaped/build.py
@@ -3,24 +3,10 @@
 import numpy as np
 from greyatomlib.python_intermediate.q01_zeros_array.build import  array_zeros
 
-def array_reshaped_zeros():    
-    zeros_array = (
-        ((1,2),
-         (3,4),
-         (5,6),
-         (7,8)),
-        ((11,12),
-         (13,14),
-         (15,16),
-         (17,18)),
-        ((11,12),
-         (13,14),
-         (15,16),
-         (17,18)),
-    )
-    d = np.array(zeros_array)
-    a = np.zeros((3,4,2)) #another array
-    d = a
-    zeros_array_reshaped = np.reshape(d, (2,3,4))
-    return zeros_array_reshaped
+def array_reshaped_zeros():
+    zeros = np.zeros([3,4,2])
+    reshape = zeros.reshape([2,3,4])
+    return (reshape)
+array_reshaped_zeros()
+
 
--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -3,8 +3,9 @@
 import numpy as np
 
 def create_3d_array():
-    A = np.arange(27).reshape((3,3,3)) #1
-    N = A.size #1
-    Z = np.arange(N) #2
-    return Z.reshape((3,3,3))
+    vals = np.arange(0,27)
+    reshapedd = vals.reshape([3,3,3])
+    return reshapedd
+create_3d_array()
+
 
