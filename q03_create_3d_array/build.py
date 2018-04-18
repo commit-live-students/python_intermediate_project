2523b0529861f24665391ce62bd42b6e11e216de
siddesh001 <siddesh.001@gmail.com>
2018-04-18 09:00:12 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -2,3 +2,8 @@
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    arr=np.zeros((3,3,3))
+    print(arr)
+    return arr
+create_3d_array()
--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,5 +1,10 @@
 # Default Imports
+import csv
 import numpy as np
 path = "./data/ipl_matches_small.csv"
 
 # Enter code here
+def read_csv_data_to_ndarray(path,dtype=np.float64):
+    a=np.genfromtxt(path,dtype,delimiter=',',skip_header=1)
+    return a
+#print(read_csv_data_to_ndarray(path))
