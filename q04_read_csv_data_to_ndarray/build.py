b2286fc621606419cf0b28792262174e91e8cdfa
sanchitaharlalka <Sanchitaharlalka@gmail.com>
2018-04-21 17:41:03 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,5 +1,6 @@
-# Default Imports
 import numpy as np
-path = "./data/ipl_matches_small.csv"
+def read_csv_data_to_ndarray(path,dtype=np.float64):
+    weather = np.genfromtxt(path, dtype=dtype, skip_header=1, delimiter=',')
 
-# Enter code here
+    return(weather)
+
