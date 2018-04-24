889631e04a0d8e5f153fbd53e2dfebfde8f1b348
gauravpednekar <grv.pednekar@gmail.com>
2018-04-24 13:45:11 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,23 @@
+# %load q07_get_unique_teams_set/build.py
 # Default imports
+import numpy as np
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
+path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+def get_unique_teams_set():
+    ipl_matches_array =  read_ipl_data_csv(path,'|S20')
+    team1 = ipl_matches_array[:,3].astype(np.object)
+    team1 = np.unique(team1)    
+#     print(team1)
+    team2 = ipl_matches_array[:,4].astype(np.object)
+    team2 = np.unique(team2)
+#     print(team2)
+    teams = np.union1d(team1,team2)
+#     print(teams)
+    teams = np.unique(teams)
+    return teams 
+#     
+    
+get_unique_teams_set()
+
--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,3 +1,4 @@
+# %load q08_get_total_extras/build.py
 # Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
@@ -5,3 +6,13 @@ import numpy as np
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+def get_total_extras():
+    ipl_matches_array =  read_ipl_data_csv(path,'|S20')
+    extras_arr = ipl_matches_array[:,17].astype(np.float16)
+    extras = extras_arr.sum()
+    return extras
+
+get_total_extras()
+    
+
+
