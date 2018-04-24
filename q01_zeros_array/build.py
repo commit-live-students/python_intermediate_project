e4d115034eb2c5c0cdf2ee4e16c61d4e8bb6c543
Sumeshks29 <sumeshks29@gmail.com>
2018-04-24 01:10:18 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,3 +1,4 @@
+# %load q01_zeros_array/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
@@ -6,4 +7,9 @@ def array_zeros():
     zeros_array = np.zeros((3,4,2))
     return zeros_array
 array_zeros()
+
 # Your solution
+
+
+
+
--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -2,3 +2,7 @@
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    arr = np.ndarray((3,3,3,))
+    print (arr)
+    
