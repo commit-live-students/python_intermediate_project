9017986f968b92e8ee177042883a19252d4cd21f
Sumeshks29 <sumeshks29@gmail.com>
2018-04-24 10:21:42 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,8 +1,17 @@
+# %load q03_create_3d_array/build.py
 # Default Imports
 import numpy as np
 
 # Enter solution here
 def create_3d_array():
-    arr = np.ndarray((3,3,3,))
-    print (arr)
-    
+    arr=np.zeros((3,3,3))
+    print(arr)
+    print(len(arr))
+    N=1
+    for dim in np.shape(arr): N *= dim
+    print(N)
+    arr2=np.arange(0,N)
+    print(arr2)
+    arr2=arr2.reshape(3,3,3)
+    return arr2
+create_3d_array()
