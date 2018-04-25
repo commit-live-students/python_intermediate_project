769f65ab0c10edcc85f542c46cf2247a5112197f
kishangooner <kishansk26@gmail.com>
2018-04-25 06:58:24 +0000

    Done

--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -7,13 +7,11 @@ path = 'data/ipl_matches_small.csv'
 array=np.array(0)
 
 def get_total_extras():
-    data=read_ipl_data_csv(path,'|S24')
+    data=read_ipl_data_csv(path,'int32')
     x=np.array(data[0:,-6])
-    for data1 in x:
-        array=x.astype(int)
     
-    return(np.sum(array))
     
-#get_total_extras()
+    return(np.sum(x))
+get_total_extras()
 # Enter Code Here
 
