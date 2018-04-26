c2daac2b2142fb814b9475fd397dab00442d1262
prasannajayashankar <prasanna.jay@gmail.com>
2018-04-26 09:36:06 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,8 +1,16 @@
+# %load q01_zeros_array/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
-
+def array_zeros():
+    zeros_array=np.arange(24)
+    zeros_array=zeros_array.reshape(3,4,2)*0
+    return zeros_array
+    
 # Your solution
+
+
+
 
 
