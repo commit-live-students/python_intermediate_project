9815189e6e61adac96492c5220bc356a2f2daa9d
bharadwajk9 <utkb.k9@gmail.com>
2018-04-27 12:36:15 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,10 @@
-# Default Imports
 import numpy as np
+from greyatomlib.python_intermediate.q01_zeros_array.build import  array_zeros
 
-# Enter solution here
+def create_3d_array():
+    arr_nd = np.arange(0,27).reshape(3,3,3)
+    return arr_nd
+create_3d_array()
+    
+
+
