10d26ca6c8da8ed49ef0daac2926353400fc531f
prasannajayashankar <prasanna.jay@gmail.com>
2018-04-28 08:50:38 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,5 +1,12 @@
+# %load q04_read_csv_data_to_ndarray/build.py
 # Default Imports
 import numpy as np
-path = "./data/ipl_matches_small.csv"
+path = './data/ipl_matches_small.csv'
+def read_csv_data_to_ndarray(path,dtype):
+    array=np.genfromtxt(path,dtype=dtype,delimiter=',',skip_header=1)
+    return array 
 
 # Enter code here
+
+
+
