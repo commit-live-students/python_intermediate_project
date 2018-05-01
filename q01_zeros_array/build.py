c74115a863ac5c1bf038f85ea5b926f7fe211c0a
kushalkk90 <kushal_kk90@yahoo.com>
2018-05-01 14:05:32 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,9 +1,9 @@
 
 import numpy as np
 def array_zeros():
-    zeros_array = np.array([3,4,2])
+    zeros_array = np.array([3,4,2]
     return np.zeros(zeros_array)
-    
+                           
 array_zeros()
 
 
