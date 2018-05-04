6d14f110f0ff06f88f99fbf75461b265e3dc6b8e
vishal-parkar <vishparks@gmail.com>
2018-05-04 10:32:09 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,10 @@
-# Default Imports
 import numpy as np
 
-# Enter solution here
+def create_3d_array():
+    temp_arr = np.ones((3,3,3))
+    N = temp_arr.size
+    Narr = np.arange(0,N).reshape(3,3,3)
+    return Narr
+
+
+
