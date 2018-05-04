2050142ca1b817cc46fb7e89d8fb0f8e683bd03d
vishal-parkar <vishparks@gmail.com>
2018-05-04 13:09:20 +0000

    Done

--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,7 +1,13 @@
+# %load q08_get_total_extras/build.py
 # Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
 
 path = 'data/ipl_matches_small.csv'
 
-# Enter Code Here
+def get_total_extras():
+    weather = np.genfromtxt(path,dtype='|S20',skip_header=True, delimiter=',')
+    return weather[:,17].astype(int).sum()
+get_total_extras()
+
+
