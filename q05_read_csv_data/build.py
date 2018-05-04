c4b6a3e70eed8ff14319963dda0a26aea749f613
vishal-parkar <vishparks@gmail.com>
2018-05-04 11:37:19 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,9 @@
-# Default imports
 import numpy as np
+path = './data/ipl_matches_small.csv'
 
-# Enter code here
+def read_ipl_data_csv(path,dtype):
+    ipl_matches_array = np.genfromtxt(path,dtype=dtype,skip_header=True, delimiter=',')
+    return ipl_matches_array
+read_ipl_data_csv(path,'|S20')
+
+
