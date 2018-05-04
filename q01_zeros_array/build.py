8e54ef0fa8cdbc1c48cb8fda9bba96bfeea3912c
sanjarora <sanjarora@gmail.com>
2018-05-04 19:55:02 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,8 +1,26 @@
-# Default Imports
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
-# Your solution
+def array_zeros(): 
+    zeros_array = np.array([[[ 0,  1],
+               [ 2,  3],
+               [ 4,  5],
+               [ 6,  7]],
+              [[ 8,  9],
+               [10, 11],
+               [12, 13],
+               [14, 15]],
+              [[16, 17],
+               [18, 19],
+               [20, 21],
+               [22, 23]]])
+    
+    zeros_array = np.zeros((3,4,2))
+    print(zeros_array)
+    return zeros_array
+    
+testArray = array_zeros()
+print(testArray.shape)
+
+
 
 
