5bc6f68cdf03b4d42be4774503a290e4f988fca3
prasannajayashankar <prasanna.jay@gmail.com>
2018-05-04 11:35:37 +0000

    Done

--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -1,5 +1,27 @@
+# %load q06_get_unique_matches_count/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
+import numpy as np
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+def get_unique_matches_count():
+    ipl_matches_array = read_ipl_data_csv('data/ipl_matches_small.csv',
+                                          dtype='|S50')
+    return len(set(ipl_matches_array[:, 0]))
+
+    
+
+ipl_matches_array = read_ipl_data_csv('data/ipl_matches_small.csv',
+                                          dtype='|S50')
+def get_unique_matches_count():
+    data=np.genfromtxt(path,delimiter=',',skip_header=1)
+    xxx = len(np.unique(data[0]))
+    return xxx
+get_unique_matches_count()
+len(np.unique(data[:,0])),data.shape
+1451/20/6
+
+len(set(ipl_matches_array[:, 0]))
+
+
--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,16 @@
+# %load q07_get_unique_teams_set/build.py
 # Default imports
+import numpy as np
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
+path = 'data/ipl_matches_small.csv'
+def get_unique_teams_set():
+    ipl_matches_array = read_ipl_data_csv('data/ipl_matches_small.csv',
+                                          dtype='|S50')
+    team1=(ipl_matches_array[:,3])
+    team2=(ipl_matches_array[:,4])
+    return set(np.union1d(team1,team2))
 
 # Enter Code Here
+
+
+
