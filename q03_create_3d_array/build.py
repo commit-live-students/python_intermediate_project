fd578802a9989b7af766b1d4d7eff675e0067b68
asdoc <asdocode@gmail.com>
2018-05-05 09:00:02 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,11 @@
+# %load q03_create_3d_array/build.py
 # Default Imports
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    ndarr = np.arange(27)
+    ndarr.shape = (3,3,3)
+    return ndarr
+
+
