ae1051032fcce924573280940e53bbe7758fde31
amol120586 <amol.24365@gmail.com>
2018-05-11 06:50:35 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,11 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
 
 # Enter code here
+path='data/ipl_matches_small.csv'
+def read_ipl_data_csv(path,dtype='|S50'):
+    ipl_matches_array = np.genfromtxt(path,dtype,delimiter=',', skip_header=1)
+    return ipl_matches_array
+
+
