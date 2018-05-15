6f842764b3c3ecf92cd42acd3d274d49a70e3da6
amol120586 <amol.24365@gmail.com>
2018-05-15 09:02:35 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,23 @@
+# %load q07_get_unique_teams_set/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
+path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+
+def get_unique_teams_set():
+    team_1 = set()
+    team_2 = set()
+    
+    data = read_ipl_data_csv(path,'|S50')
+
+    for i in range(0,data.shape[0]):
+        if data[i][3] not in team_1:
+                team_1.add(data[i][3])
+        if data[i][4] not in team_2:
+                team_2.add(data[i][4])
+        
+    return (team_1.union(team_2))
+get_unique_teams_set()
+
+
--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,3 +1,4 @@
+# %load q08_get_total_extras/build.py
 # Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
@@ -5,3 +6,14 @@ import numpy as np
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+
+def get_total_extras():
+    extra=0
+    data=read_ipl_data_csv(path,'|S50')
+    
+    for i in range(0,data.shape[0]):
+        extra += int(data[i][17])
+    return extra
+    
+
+
