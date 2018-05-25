64a8d34360b1ec0329f43e10e10a7b5c4a66ef34
SL345 <sherilyn345@gmail.com>
2018-05-25 05:58:40 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -5,5 +5,5 @@ path = './data/ipl_matches_small.csv'
 
 # Enter code here
 def read_csv_data_to_ndarray(path, dtype):
-    return np.genfromtxt(path, dtype, skip_header = 1)
+    return np.genfromtxt(path, dtype,delimiter=',', skip_header = 1)
 
