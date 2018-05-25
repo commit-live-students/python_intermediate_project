f93e49c55b69ba52e6c0f54a21d52654d43184ac
SL345 <sherilyn345@gmail.com>
2018-05-25 07:30:29 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,13 @@
+# %load q07_get_unique_teams_set/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
+path = 'data/ipl_matches_small.csv'
+import numpy as np
 
 # Enter Code Here
+def get_unique_teams_set():
+    ipl_matches = read_ipl_data_csv(path, dtype='|S50')
+    team1=np.unique(ipl_matches[:,3])
+    team2=np.unique(ipl_matches[:,4])
+    return set(np.union1d(team1,team2))
+
