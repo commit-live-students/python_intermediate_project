146a1b1686a07654e2e0403c04cb328fee7763dd
geraldhood <gerald.hoody@gmail.com>
2018-06-04 11:47:26 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,8 +1,25 @@
-# Default Imports
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
-import numpy as np
 
-# Your solution
+from greyatomlib.python_getting_started.q01_read_data.build import read_data
+data = read_data()
+
+def extras_runs(data=data):
+
+    first_innings_deliveries = data['innings'][0]['1st innings']['deliveries']
+    extras_1st_innings = [delivery_info
+                          for delivery in first_innings_deliveries
+                          for delivery_number, delivery_info in delivery.items()
+                          if 'extras' in delivery_info]
+    print(len(extras_1st_innings))
+
+    second_innings_deliveries = data['innings'][1]['2nd innings']['deliveries']
+    extras_2nd_innings = [delivery_info
+                          for delivery in second_innings_deliveries
+                          for delivery_number, delivery_info in delivery.items()
+                          if 'extras' in delivery_info]
+    print(len(extras_2nd_innings))
+
+    difference = len(extras_2nd_innings) - len(extras_1st_innings)
+
+    return difference
 
 
--- q02_zeros_reshaped/build.py
+++ q02_zeros_reshaped/build.py
@@ -1,5 +1,10 @@
+
 # Default imports
 import numpy as np
 from greyatomlib.python_intermediate.q01_zeros_array.build import  array_zeros
 
-# Write your code
+
+def array_reshaped_zeros():
+    ans = array_zeros()
+    return ans.reshape(2,3,4)
+
