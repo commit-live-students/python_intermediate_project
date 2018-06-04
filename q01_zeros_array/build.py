a27ec803b7800468c9d9af4f5aac69bcbd3179c3
geraldhood <gerald.hoody@gmail.com>
2018-06-04 11:57:57 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,25 +1,8 @@
 
-from greyatomlib.python_getting_started.q01_read_data.build import read_data
-data = read_data()
+import numpy as np
 
-def extras_runs(data=data):
-
-    first_innings_deliveries = data['innings'][0]['1st innings']['deliveries']
-    extras_1st_innings = [delivery_info
-                          for delivery in first_innings_deliveries
-                          for delivery_number, delivery_info in delivery.items()
-                          if 'extras' in delivery_info]
-    print(len(extras_1st_innings))
-
-    second_innings_deliveries = data['innings'][1]['2nd innings']['deliveries']
-    extras_2nd_innings = [delivery_info
-                          for delivery in second_innings_deliveries
-                          for delivery_number, delivery_info in delivery.items()
-                          if 'extras' in delivery_info]
-    print(len(extras_2nd_innings))
-
-    difference = len(extras_2nd_innings) - len(extras_1st_innings)
-
-    return difference
+def array_zeros():
+    zeros_array = np.zeros((3, 4, 2))
+    return zeros_array
 
 
