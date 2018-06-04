f077122ba0b589c8906e814f5623ef14eca19292
geraldhood <gerald.hoody@gmail.com>
2018-06-04 11:50:50 +0000

    Done

--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -1,5 +1,8 @@
-# Default imports
-from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = 'data/ipl_matches_small.csv'
 
-# Enter Code Here
+# Default imports
+import numpy as np
+
+def read_ipl_data_csv(path, dtype):
+
+    return np.genfromtxt(path, dtype=dtype, skip_header=1, delimiter=',')
+
