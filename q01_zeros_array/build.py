75c236d4d6fe1bc7a29fbba2f4d5ef5e6a4b54fe
amagarwal-18 <amitagarwal.skit@gmail.com>
2018-07-19 18:09:13 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,8 +1,19 @@
+# %load q01_zeros_array/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
+def array_zeros():
+    zeros_array = np.zeros(shape=(3,4,2))
+    return zeros_array
 # Your solution
+
+var = array_zeros()
+print(var.shape)
+print(var.max())
+
+
+
 
 
