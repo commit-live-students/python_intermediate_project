4b4a5ba5a4c5d2c252f8cb2a777e1ac5f4e69950
Tusharsharma86 <tusharsharma86@gmail.com>
2018-07-19 18:50:34 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,8 +1,18 @@
+# %load q01_zeros_array/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
 # Your solution
+def array_zeros():
+    #return zero array
+    zeros_array = np.random.randint(1,size=(3,4,2))
+    return zeros_array
+
+
+if __name__ == '__main__':
+    array_zeros()
+    print(array_zeros())
 
 
