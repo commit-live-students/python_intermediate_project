386b25e19d7501f42a039fb6713a593fcb2e779d
sagarpatil232 <sagarpatil232@gmail.com>
2018-07-22 18:02:20 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,22 @@
+# %load q03_create_3d_array/build.py
 # Default Imports
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    list_1 = [
+            [[0, 1, 2],
+             [3, 4, 5],
+             [6, 7, 8]],
+
+            [[9, 10, 11],
+             [12, 13, 14],
+             [15, 16, 17]],
+
+            [[18, 19, 20],
+             [21, 22, 23],
+             [24, 25, 26]]
+        ]
+    np.reshape(list_1, (3,3,3))
+    return list_1
+
