490e822998bc39311e6cb46f742c9631299a9c95
rianboy18 <pranavr95.pn@gmail.com>
2018-07-22 08:37:38 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,8 +1,19 @@
+# %load q01_zeros_array/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
+
 # Your solution
+def array_zeros():
+    zeros_array=np.zeros([3,4,2])
+    return zeros_array
+
+
+a=array_zeros()
+a
+print(type(a))
+a.shape
 
 
--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -1,5 +1,17 @@
+# %load q06_get_unique_matches_count/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 path = 'data/ipl_matches_small.csv'
-
+import numpy as np
 # Enter Code Here
+def get_unique_matches_count():
+    a=np.genfromtxt('data/ipl_matches_small.csv',delimiter=',')
+    b=np.unique(a[:,0])
+    c=b[~np.isnan(b)]
+    ipl_matches_array=np.prod(c.shape)
+    return ipl_matches_array
+
+c=get_unique_matches_count()
+c
+
+
