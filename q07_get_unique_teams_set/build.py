7117dbe5f13f585136259c6f79fdac8413aa7b20
kaushik1975 <kaushik1975@gmail.com>
2018-07-24 14:16:46 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,20 @@
+# %load q07_get_unique_teams_set/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
+import numpy as np
+
+path = 'data/ipl_matches_small.csv'
+dtype = '|S100'
 
 # Enter Code Here
+def read_ipl_data_csv():
+    ipl_data_csv = np.genfromtxt(path,dtype,skip_header=1,delimiter=',')
+    t_team1 = ipl_data_csv[:,3]
+    t_team2 = ipl_data_csv[:,4]
+    team1 = np.unique(t_team1)
+    team2=np.unique(t_team2)
+    return np.array(np.union1d(team1,team2))
+
+
+
+
--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,7 +1,15 @@
+# %load q08_get_total_extras/build.py
 # Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
 
 path = 'data/ipl_matches_small.csv'
+dtype = '|S100'
 
 # Enter Code Here
+def get_total_extras():
+    arrTotalExtras = np.genfromtxt(path,dtype,skip_header=1,delimiter=',')
+    intTotalExtras = arrTotalExtras[:,17].astype(np.int16)
+    return intTotalExtras.sum()
+
+
