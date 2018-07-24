c9870079aa6182a66e90c04621c1b0fd337b78c0
sahusaurabh65 <sahusaurabh65@gmail.com>
2018-07-24 11:39:02 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,11 @@
+# %load q03_create_3d_array/build.py
 # Default Imports
 import numpy as np
-
+def create_3d_array():
+    ndr = np.arange(27)
+    ndr.shape = (3,3,3)
+    return ndr
 # Enter solution here
+create_3d_array()
+
+
