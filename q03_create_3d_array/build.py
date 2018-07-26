c365eab46afd88b0dedb618fbfa9b619d5425a59
Tusharshah2006 <tusharshah2006@gmail.com>
2018-07-26 18:05:25 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,12 @@
+# %load q03_create_3d_array/build.py
 # Default Imports
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    ndimarray = np.arange(0, 27)
+    ndimarray.shape = (3, 3, 3)
+    
+    return ndimarray
+
+
