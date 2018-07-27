c3b4bddd716db7ae83fbdfdb55e0c7ce237d6584
Akashdesarda <desardaakash@gmail.com>
2018-07-27 15:30:19 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,11 +1,11 @@
 # %load q04_read_csv_data_to_ndarray/build.py
 # Default Imports
 import numpy as np
-path = './data/ipl_matches_small.csv'
 
 # Enter code here
 def read_csv_data_to_ndarray():
-    a = np.genfromtxt(path,delimiter=',')
+    path = './data/ipl_matches_small.csv'
+    a = np.genfromtxt(path,delimiter=',',skip_header=1,dtype=s)
     return a 
 
 
--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,7 +1,15 @@
+# %load q08_get_total_extras/build.py
 # Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
-
+import pandas as pd
 path = 'data/ipl_matches_small.csv'
-
+data = pd.read_csv(path)
 # Enter Code Here
+def get_total_extras():
+    a = data['extras'].sum()
+    return a
+b= get_total_extras()
+b
+
+
