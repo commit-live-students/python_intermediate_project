b38f70fbc1391b8f2b31ec1110dd1f3ac20f0b34
sach07 <sachinsg873@gmail.com>
2018-07-29 16:04:32 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,5 +1,18 @@
+# %load q04_read_csv_data_to_ndarray/build.py
 # Default Imports
 import numpy as np
-path = "./data/ipl_matches_small.csv"
+from inspect import getfullargspec
+path = 'data/ipl_matches_small.csv'
+
+def read_csv_data_to_ndarray(path,input_dtype = np.float64):
+   data = np.genfromtxt(path,dtype = input_dtype,delimiter = ',',skip_header = 1)
+   #args = getfullargspec(read_csv_data_to_ndarray)
+   #print(len(args[0]))
+   #print(data.shape)
+   #print(data.dtype)
+   return data
 
 # Enter code here
+#read_csv_data_to_ndarray(path,'|S100')
+
+
--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,10 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
-
+def read_ipl_data_csv(path='data/ipl_matches_small.csv', dtype=np.float64):
+    ipl_matches_array=np.genfromtxt(path,dtype,delimiter=',',skip_header=1)
+    return ipl_matches_array
+#read_ipl_data_csv()
 # Enter code here
+
+
