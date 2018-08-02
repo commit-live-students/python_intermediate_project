affbe43063820d01d1dd4e3025de0997d8a23aa7
ankutty <akutty@deloitte.com>
2018-08-02 10:16:38 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,9 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
 
 # Enter code here
+def read_ipl_data_csv(path, dtype):
+
+    return np.genfromtxt(path, dtype=dtype, skip_header=1, delimiter=',')
+
