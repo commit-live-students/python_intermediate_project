6c614eb9abec21d217b47a475ee01238afb2bb7a
ankutty <akutty@deloitte.com>
2018-08-02 08:15:01 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,11 @@
+# %load q03_create_3d_array/build.py
 # Default Imports
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    data = np.arange(27).reshape(3,3,3)
+    return data
+
+
+
