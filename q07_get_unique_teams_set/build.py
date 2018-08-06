2542f1f53e83f112bb0026fd9ccc8efdf9a5073d
KamalShillongwala <qamaldas@gmail.com>
2018-08-06 17:16:31 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,15 @@
+# %load q07_get_unique_teams_set/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
+path = 'data/ipl_matches_small.csv'
+import numpy as np
 
 # Enter Code Here
+def get_unique_teams_set():
+    ipl_matches_array = np.genfromtxt('./data/ipl_matches_small.csv', dtype='|S50', delimiter=',', skip_header=1)
+    unique_teams1 = np.unique(ipl_matches_array[:, 3])
+    unique_teams2 = np.unique(ipl_matches_array[:, 4])
+    teams_union = np.union1d(unique_teams1, unique_teams2)
+    return teams_union
+
+
--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,7 +1,15 @@
+# %load q08_get_total_extras/build.py
 # Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
 
 path = 'data/ipl_matches_small.csv'
-
+extras = 0
 # Enter Code Here
+def get_total_extras():
+    ipl_matches_array = np.genfromtxt('data/ipl_matches_small.csv', delimiter=',', skip_header=1)
+    extras_col = ipl_matches_array[:, 17]
+    runs = np.sum(extras_col, axis=0)
+    return runs
+
+
