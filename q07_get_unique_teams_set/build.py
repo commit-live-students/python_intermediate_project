fe400d677069c905ce7435edaa2c4852729cc3cc
Akash Desarda <desardaakash@gmail.com>
2018-08-12 12:00:47 +0530

    Update build.py

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,15 +1,13 @@
 # %load q07_get_unique_teams_set/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = 'data/ipl_matches_small.csv'
+path = "data/ipl_matches_small.csv"
 
 # Enter Code Here
 import numpy as np
 import pandas as pd
 data = pd.read_csv(path)
 def get_unique_teams_set():
-    a = {b'Deccan Chargers', b'Kings XI Punjab', b'Chennai Super Kings', b'Mumbai Indians',
-                  b'Rajasthan Royals', b'Pune Warriors', b'Kolkata Knight Riders'}
-    return a
-
-
+    a = data['team1'].unique().tolist()
+    b = data['team2'].unique().tolist()
+    return set().union(a,b)
