dca632c088a20a90f5fb43e3faafd8c7ad139da8
apurvi02 <apurvij@gmail.com>
2018-08-13 16:37:28 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,5 +1,12 @@
+# %load q04_read_csv_data_to_ndarray/build.py
 # Default Imports
 import numpy as np
-path = "./data/ipl_matches_small.csv"
+path = './data/ipl_matches_small.csv'
 
 # Enter code here
+def read_csv_data_to_ndarray(path,dtype=np.float64):
+    data=np.genfromtxt(path,dtype='|S100',delimiter=',',skip_header=1)
+    print(data.shape)
+    return data
+read_csv_data_to_ndarray(path)
+
