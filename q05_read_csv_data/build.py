4e8047860606ca55719c1579315e85fa63542907
shehzaan07 <shehzaan07@gmail.com>
2018-08-15 10:51:50 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,14 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
 
+def read_ipl_data_csv(path, dtype=np.float64):
+    ipl_matches_array = np.genfromtxt(path, delimiter=',',dtype=dtype, skip_header=1)
+    return ipl_matches_array
+
+read_ipl_data_csv('./data/ipl_matches_small.csv','|S50')
+
 # Enter code here
+
+
+
