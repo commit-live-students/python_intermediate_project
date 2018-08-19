8f884d17c741a50c1875cc50a370508c5f4074c6
sskewal <sskewal@yahoo.in>
2018-08-19 09:09:21 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,5 +1,22 @@
+# %load q04_read_csv_data_to_ndarray/build.py
 # Default Imports
 import numpy as np
-path = "./data/ipl_matches_small.csv"
+import csv
+import os
+
+
+ 
+path = 'data/ipl_matches_small.csv'
+dtype = '|S100'
+
+def read_csv_data_to_ndarray(path,dtype):
+    arr1= np.genfromtxt(path,delimiter=',',dtype='|S100',skip_header=1)
+    return arr1
+
+read_csv_data_to_ndarray(path,dtype)
+
+# print(os.getcwd())
 
 # Enter code here
+
+
--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,14 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
 
+def read_ipl_data_csv():
+    path = './data/ipl_matches_small.csv'
+    ipl_matches_array = np.genfromtxt(path, delimiter=',',dtype='|S50')
+    return ipl_matches_array
+
+read_ipl_data_csv()
+    
 # Enter code here
+
+
