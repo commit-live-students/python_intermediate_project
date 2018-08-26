6f582a7140a9f36f0c212a6479f3c8927c124841
nehatanwani <neha29616@gmail.com>
2018-08-26 17:05:03 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -1,8 +1,20 @@
+# %load q01_zeros_array/build.py
 # Default Imports
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
 
 # Your solution
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
+import numpy as np
+
+# Your solution
+def array_zeros():
+    return np.zeros(shape=(3,4,2))
+
+
+
+
 
 
