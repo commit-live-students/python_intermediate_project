b1508f1b9212286aec9145cfcf8a668303f4eb2b
SanjivMehra26 <sanjiv.mehara@gmail.com>
2018-08-29 19:07:48 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,13 @@
+# %load q07_get_unique_teams_set/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
-
+path = 'data/ipl_matches_small.csv'
+import pandas as pd 
 # Enter Code Here
+def get_unique_teams_set ():
+    team1 = pd.DataFrame(read_ipl_data_csv('data/ipl_matches_small.csv','|S100'))[3]
+    team2 = pd.DataFrame(read_ipl_data_csv('data/ipl_matches_small.csv','|S100'))[4]
+    return set(team1).union(set(team2))
+
+
+
