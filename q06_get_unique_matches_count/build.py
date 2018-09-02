ab737a810716c768a76c6e05519d19742a69454f
prodigy803 <pushkaraj803@gmail.com>
2018-09-02 06:24:26 +0000

    Done

--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -1,5 +1,23 @@
+# %load q06_get_unique_matches_count/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+import numpy as np
+import pandas as pd
+
+def get_unique_matches_count():
+    df = pd.read_csv(path)
+    list1 = []
+    for i in range(0,len(df['match_code'])):
+        match_code_1 = df['match_code'][i]
+        if match_code_1 not in list1:
+            list1.append(match_code_1)
+    
+    return len(list1)
+
+
+
+
+
