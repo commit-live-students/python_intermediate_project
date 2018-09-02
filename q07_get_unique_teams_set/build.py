3f02c634be5eb02e92c74fa40c5571900457761c
prodigy803 <pushkaraj803@gmail.com>
2018-09-02 07:07:04 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,26 @@
+# %load q07_get_unique_teams_set/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
+path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+import pandas as pd
+column_1 = set()
+column_2 = set()
+def get_unique_teams_set():
+    df = pd.read_csv(path)
+    for i in range(0,len(df['match_code'])):
+        team1 = df['team1'][i]
+        team2 = df['team2'][i]
+        if team1 not in column_1:
+            column_1.add(team1)
+            
+        if team2 not in column_2:
+            column_2.add(team2)
+    
+    return column_1 | column_2
+
+
+
+
+
--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,3 +1,4 @@
+# %load q08_get_total_extras/build.py
 # Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
@@ -5,3 +6,16 @@ import numpy as np
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+import numpy as np
+import pandas as pd
+
+def get_total_extras():
+    df = pd.read_csv(path)
+    count = 0
+    for i in range(0,len(df['extras'])):
+        count = count + df['extras'][i]
+    
+    return count
+#get_total_extras()
+
+
