0a8f90824a3effe763110aa220a46d690f77205a
sreelekhaWoonna <sreelekha4877@gmail.com>
2018-09-03 18:13:22 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,9 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
 
 # Enter code here
+def read_ipl_data_csv(path,dtype=np.float64):
+    return np.genfromtxt(path,skip_header=1, delimiter=',', dtype=dtype)
+
+
