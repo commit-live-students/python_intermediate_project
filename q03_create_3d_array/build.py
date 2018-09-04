2c109f869f013f629bddd44657a80e34e4fbd058
hemant-hingorani <hemanthingorani@yahoo.com>
2018-09-04 07:29:15 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,17 @@
+# %load q03_create_3d_array/build.py
 # Default Imports
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+
+    array1=np.arange(27)
+    print (array1)
+    array3d=array1.reshape(3,3,3)
+    print (array3d)
+    return(array3d)
+    
+    
+create_3d_array()
+
+
--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,13 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
+path = './data/ipl_matches_small.csv'
 
 # Enter code here
+def read_ipl_data_csv(path, dty):
+    ipl_matches_array=np.genfromtxt(path, dtype=dty, skip_header=1, delimiter=',')
+    return(ipl_matches_array)
+    
+read_ipl_data_csv(path, '|S50')
+
+
