90fb410256bf35ed80db068eb91019ff424f3e98
Abhimanyu22 <link2manyu@hotmail.com>
2018-09-06 18:02:27 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,20 @@
-# Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
+path = './data/ipl_matches_small.csv'
+import numpy as np
 
-# Enter Code Here
+def get_unique_teams_set():
+    arr = np.genfromtxt(path, dtype = '|S50', skip_header=1, delimiter=',')
+    arr1 = arr[:,3]
+    arr2 = arr[:,4]
+    arr3 = np.unique(arr1)
+    arr4 = np.unique(arr2)
+    read_unique = np.union1d(arr3, arr4)
+    print(read_unique)
+    
+    return
+
+
+
+
+
+
--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,7 +1,17 @@
-# Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
 
-path = 'data/ipl_matches_small.csv'
+path = './data/ipl_matches_small.csv'
 
-# Enter Code Here
+def get_total_extras():
+    path = './data/ipl_matches_small.csv'
+    arr = np.genfromtxt(path, dtype = 'int', skip_header=1, delimiter=',')
+    total = sum(arr[:,17])
+    
+    return total
+    
+
+
+
+
+
