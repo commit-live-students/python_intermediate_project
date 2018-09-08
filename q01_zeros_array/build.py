81d510ad8ea256feb4b9c7044d6a27b5de01e77c
aksmungekar <mungekarakshay@yahoo.com>
2018-09-08 17:07:21 +0000

    Done

--- q01_zeros_array/build.py
+++ q01_zeros_array/build.py
@@ -3,12 +3,21 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir),  '..'  ))
 import numpy as np
+def array_zero():
+    
+    zeros_array=np.zeros((3,4,2))
+    
+    return zeros_array
+array_zero
 def array_zeros():
     zeros_array=np.zeros((3,4,2))
     return zeros_array
-  n=(3,4,2)
-zeros_array=np.array(n)
-type(zeros_array)
-np.zero(4)
+def xyc():
+    ba=np.zeros((3,4,2))
+    return ba
+xyc
+
+
+
 
 
