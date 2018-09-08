8a1156c8c4fc38fa1d184a05b8edc21e6dadc239
savans7 <connecttosavan@gmail.com>
2018-09-08 01:51:47 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,7 @@
-# Default Imports
 import numpy as np
+def create_3d_array() :
+    array=np.array(range(0,27))
+    size=array.size
+    array_reshaped=array.reshape(3,3,3)
+    return array_reshaped
 
-# Enter solution here
