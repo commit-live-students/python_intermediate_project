725a9e1c07ad524b4db1358da9de23d6d2129f03
prodigy803 <pushkaraj803@gmail.com>
2018-09-08 15:02:46 +0000

    Done

--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -7,17 +7,24 @@ path = 'data/ipl_matches_small.csv'
 import numpy as np
 import pandas as pd
 
+
 def get_unique_matches_count():
+    
+    ''' Solution 1:
     df = pd.read_csv(path)
     list1 = []
     for i in range(0,len(df['match_code'])):
         match_code_1 = df['match_code'][i]
         if match_code_1 not in list1:
             list1.append(match_code_1)
+    '''
     
-    return len(list1)
+    #Solution 2:
+    ipl_matchs_array = np.genfromtxt(path,dtype=int,skip_header=1,delimiter=',')
+    return len(np.unique(ipl_matchs_array[:,0]))
+#ipl_matchs_array
+#import numpy as np
 
-
-
+#len(np.unique(np.genfromtxt(path,dtype=int,skip_header=1,delimiter=',')[:,0]))
 
 
