0c5ad87007b14eb270e375cd7ac1cceaa22e09fe
dhananjay93 <dhananjay.hawal@gmail.com>
2018-09-09 16:40:10 +0000

    Done

--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,3 +1,4 @@
+# %load q08_get_total_extras/build.py
 # Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
@@ -5,3 +6,11 @@ import numpy as np
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+def get_total_extras() :
+    file1 = np.sum(np.genfromtxt(path,skip_header = 1 , delimiter=',')[:,-6],axis=0)
+    return file1
+    
+
+
+
+
