6aff719022fb8a8b07a85e29fad0b7bbecffa27e
savans7 <connecttosavan@gmail.com>
2018-09-10 02:50:21 +0000

    Done

--- q06_get_unique_matches_count/build.py
+++ q06_get_unique_matches_count/build.py
@@ -1,5 +1,22 @@
-# Default imports
-from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = 'data/ipl_matches_small.csv'
+import numpy as np
+import csv
+def get_unique_matches_count() :
+    ipl_matches_arrays = (','.join(i) for i in csv.reader(open('data/ipl_matches_small.csv','r')))   # ipl_matches_array=np.genfromtxt('/Users/saravanan/Documents/GIT/GreayAtom-Assignment/sara_Sheet1.csv',
+    arrays=[]
+    for row in ipl_matches_arrays:
+        list1=row.split(',');
+        #del list1[0]
+        #del list1[0]
+        #del list1[10]
+        #print(list1)
+        arrays.append(list1)
+    
+    #clean= np.genfromtxt(arrays,invalid_raise=False,dtype=None,delimiter='\,')
 
-# Enter Code Here
+    arrays=np.array(arrays)[1:]
+    #print(np.unique(arrays[:,16:17],axis=0))
+    ipl_matches_array=len(np.unique(arrays[:,1:2],axis=0))
+    return ipl_matches_array
+
+
+
