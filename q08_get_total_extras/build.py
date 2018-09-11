7803a35e62bd9344dda579cc605fbce4eac88bc0
savans7 <connecttosavan@gmail.com>
2018-09-11 02:39:18 +0000

    Done

--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,7 +1,32 @@
-# Default Imports
-from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
+import csv
 import numpy as np
 
-path = 'data/ipl_matches_small.csv'
+def read_ipl_data_csv():
+    ipl_matches_arrays = (','.join(i) for i in csv.reader(open('data/ipl_matches_small.csv','r')))   # ipl_matches_array=np.genfromtxt('/Users/saravanan/Documents/GIT/GreayAtom-Assignment/sara_Sheet1.csv',
+    arrays=[]
+    for row in ipl_matches_arrays:
+        list1=row.split(',');
+        arrays.append(list1)
+        
+    arrays=np.array(arrays)[1:]
+    return arrays
 
-# Enter Code Here
+def get_total_extras() :
+    matchInfo=read_ipl_data_csv()
+    extrasList=[int(s) for s in matchInfo[:,17:18]]
+   # print ([int(s) for s in matchInfo[:,18:19]])
+   # print(extrasList)
+    extras=np.sum(extrasList)
+    return extras
+   # print(get_total_extras())
+
+    
+     
+
+
+
+
+
+
+
+
