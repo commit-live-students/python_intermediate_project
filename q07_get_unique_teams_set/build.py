e8cf80c8a7b337e5f9cca8cb403825536f285997
savans7 <connecttosavan@gmail.com>
2018-09-11 01:55:58 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,26 @@
-# Default imports
-from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
+import numpy as np
+import csv
 
-# Enter Code Here
+def read_ipl_data_csv():
+    ipl_matches_arrays = (','.join(i) for i in csv.reader(open('data/ipl_matches_small.csv','r')))   # ipl_matches_array=np.genfromtxt('/Users/saravanan/Documents/GIT/GreayAtom-Assignment/sara_Sheet1.csv',
+    arrays=[]
+    for row in ipl_matches_arrays:
+        list1=row.split(',');
+        arrays.append(list1)
+    arrays=np.array(arrays)[1:]
+    return arrays
+
+
+def get_unique_teams_set():
+    matchInfo=read_ipl_data_csv()
+    teams1=np.unique(matchInfo[:,4:5])
+    teams2=np.unique(matchInfo[:,5:6])
+    uteams=np.union1d(teams1,teams2)
+    #print(uteams)
+    teams=set()
+    teams.add('Kolkata Knight Riders'.encode('ASCII'))
+    for x in set(uteams) :
+        teams.add(x.encode('ASCII'))
+        
+    return teams
+
