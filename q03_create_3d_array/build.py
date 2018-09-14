71bb66163482d52c65f1b70d553a2943f148f207
rnoshir <noshir92@gmail.com>
2018-09-14 14:18:02 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,8 +1,27 @@
+# %load q03_create_3d_array/build.py
 # Default Imports
 import numpy as np
 
 # Enter solution here
 def create_3d_array():
-    n=3**3
-    arr=np.array(range(n)).reshape((3,3,3))
-    return arr
+    n = 27
+    arr = np.array(range(n))
+    return arr.reshape((3,2,2))
+
+actual = [
+            [[0, 1, 2],
+             [3, 4, 5],
+             [6, 7, 8]],
+
+            [[9, 10, 11],
+             [12, 13, 14],
+             [15, 16, 17]],
+
+            [[18, 19, 20],
+             [21, 22, 23],
+             [24, 25, 26]]
+        ]
+arr = create_3d_array()
+
+np.all(arr == actual)
+
