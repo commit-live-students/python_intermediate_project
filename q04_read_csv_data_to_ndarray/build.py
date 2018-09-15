de9ca1d89d42b1173b96bba3ff827cf80d2866d5
sunilhariharan <sunilhariharan@gmail.com>
2018-09-15 14:06:09 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,5 +1,16 @@
+# %load q04_read_csv_data_to_ndarray/build.py
 # Default Imports
 import numpy as np
-path = "./data/ipl_matches_small.csv"
 
+path = './data/ipl_matches_small.csv'
+
+
+def read_csv_data_to_ndarray(path,dtype=np.float64):
+    array=np.genfromtxt(path, dtype=dtype,delimiter=',',skip_header=1)
+    
+    return array
+    
+    
 # Enter code here
+
+
--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,15 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
 
+path = './data/ipl_matches_small.csv'
+
+def read_ipl_data_csv(path,dtype=np.float64):
+    
+    ipl_matches_array=array=np.genfromtxt(path, dtype='|S50',delimiter=',',skip_header=1)
+    
+    return ipl_matches_array
+
 # Enter code here
+
+
