c950a5f018118505a01fcc67b405e159f6f28bc0
aksmungekar <mungekarakshay@yahoo.com>
2018-09-15 15:03:27 +0000

    Done

--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,7 +1,37 @@
+# %load q08_get_total_extras/build.py
 # Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
+import csv
 
 path = 'data/ipl_matches_small.csv'
+def get_total_extras():
+    
+    
+    a = np.genfromtxt(path,dtype=None,delimiter=',',skip_header=1) #readfile
+    
+   
+    extras = []  #Creating list to store extra runs.
+    
+    
+    for i,v in enumerate(a): #looping through list
+        extras.append(a[i][17])  #extras column index(17th is the order)
+    return sum(extras)
+get_total_extras()
 
-# Enter Code Here
+        
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
