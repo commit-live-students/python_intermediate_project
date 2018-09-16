f9ca1fa51dade4d15deab6ab836a6aed2d07426f
simpleguyon <bhandaru.dheeraj@gmail.com>
2018-09-16 08:52:05 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -4,13 +4,13 @@ import numpy as np
 path = 'data/ipl_matches_small.csv'
 def read_ipl_data_csv(path,dtype):
     
-    ipl_matches_array = np.genfromtxt('data/ipl_matches_small.csv', dtype, delimiter=',')
-    ipl_matches_array = np.array(ipl_matches_array)
-    return(ipl_matches_array)
+    ipl_matches_array = np.genfromtxt(path, dtype,skip_header=1, delimiter=',')
+    ipl_matches_array = np.array(ipl_matches_array) 
+    return ipl_matches_array
     
 
 
-read_ipl_data_csv('data/ipl_matches_small.csv','|S50')
+read_ipl_data_csv('data/ipl_matches_small.csv', '|S50')
 
 
 
