19b96a89e811f075d0e28209ab6a954110946f3b
mario-the-legend <mario_the_legend@yahoo.co.in>
2018-09-22 10:58:14 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,15 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
 
+def read_ipl_data_csv(path,dtype):
+    #path='./data/ipl_matches_small.csv'
+    ipl_matches_array=np.genfromtxt(path, dtype=dtype, delimiter=',', skip_header=1)
+    return ipl_matches_array
+
 # Enter code here
+
+read_ipl_data_csv('./data/ipl_matches_small.csv','|S50')
+
+
+
