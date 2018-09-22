cc32be9ce3420c248e8ba7b522ac1929f508e112
mario-the-legend <mario_the_legend@yahoo.co.in>
2018-09-22 05:55:33 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -1,4 +1,15 @@
+# %load q03_create_3d_array/build.py
 # Default Imports
 import numpy as np
 
 # Enter solution here
+def create_3d_array():
+    N = 27
+    return np.arange(N).reshape(3,3,3)
+    
+    
+    
+    
+create_3d_array()
+
+
