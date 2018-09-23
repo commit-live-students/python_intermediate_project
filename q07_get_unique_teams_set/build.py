c3308c683ca6f91e17c25769380bf79cbc2adb59
mario-the-legend <mario_the_legend@yahoo.co.in>
2018-09-23 07:56:30 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -1,5 +1,16 @@
+# %load q07_get_unique_teams_set/build.py
 # Default imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
-path = "data/ipl_matches_small.csv"
+path = 'data/ipl_matches_small.csv'
+import numpy as np
 
 # Enter Code Here
+def get_unique_teams_set():
+    varfile=np.genfromtxt(path,delimiter=',',skip_header=1,dtype='|S50')
+    var1=np.unique(varfile[:,3])
+    var2=np.unique(varfile[:,4])
+    return np.union1d(var1,var2)
+    
+get_unique_teams_set()
+
+
--- q08_get_total_extras/build.py
+++ q08_get_total_extras/build.py
@@ -1,3 +1,4 @@
+# %load q08_get_total_extras/build.py
 # Default Imports
 from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
 import numpy as np
@@ -5,3 +6,9 @@ import numpy as np
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
+def get_total_extras():
+    varfile=np.genfromtxt(path,delimiter=',',skip_header=1,dtype='int16')
+    return np.sum(varfile[:,17])
+get_total_extras()
+
+
