26e6d82e2ac9d0935adc308a17a66ab379072185
mario-the-legend <mario_the_legend@yahoo.co.in>
2018-09-23 08:26:10 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -9,7 +9,7 @@ def get_unique_teams_set():
     varfile=np.genfromtxt(path,delimiter=',',skip_header=1,dtype='|S50')
     var1=np.unique(varfile[:,3])
     var2=np.unique(varfile[:,4])
-    return np.union1d(var1,var2)
+    return set(np.union1d(var1,var2))
     
 get_unique_teams_set()
 
