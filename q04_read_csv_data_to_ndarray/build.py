4c666f8b557528d27401ffe3818a8e9f4f23903c
Lavi1920 <lavitha1920@gmail.com>
2018-09-27 10:44:52 +0000

    Done

--- q04_read_csv_data_to_ndarray/build.py
+++ q04_read_csv_data_to_ndarray/build.py
@@ -1,5 +1,16 @@
-# Default Imports
+# %load q04_read_csv_data_to_ndarray/build.py
 import numpy as np
-path = "./data/ipl_matches_small.csv"
+path = './data/ipl_matches_small.csv'
+dtype = float
+def read_csv_data_to_ndarray():
+    my_data = np.genfromtxt(path,dtype=dtype,skip_header =1,delimiter=',')
+    return my_data
+
+read_csv_data_to_ndarray()
 
 # Enter code here
+
+T
+
+
+
--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,14 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
+path = './data/ipl_matches_small.csv'
+dtype = float
+def read_ipl_data_csv(path,dtype):
+    ipl_array = np.genfromtxt(path,dtype=dtype,skip_header=1,delimiter=',')
+    return ipl_array
+
 
 # Enter code here
+
+
+
