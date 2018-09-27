aa49484783d60b05c6b5708058cd67c8f008fcd0
sannidh <soniknitr@gmail.com>
2018-09-27 04:03:10 +0000

    Done

--- q03_create_3d_array/build.py
+++ q03_create_3d_array/build.py
@@ -3,8 +3,6 @@
 import numpy as np
 def create_3d_array():
     N=27
-    new_array=np.arange(0,27).reshape(3,3,3)
-    return new_array
-# Enter solution here
+    return np.arange(0,27).reshape(3,3,3)
 
 
