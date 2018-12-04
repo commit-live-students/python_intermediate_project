ff579ecce80d63a9f7eb4fb717e0c21928373571
nehatanwani <neha29616@gmail.com>
2018-12-04 10:43:16 +0000

    Done

--- q07_get_unique_teams_set/build.py
+++ q07_get_unique_teams_set/build.py
@@ -4,9 +4,16 @@ from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_dat
 path = 'data/ipl_matches_small.csv'
 
 # Enter Code Here
-def get_unique_matches_count():
-    data = read_ipl_data_csv(path,'|S100')
-    data = data[:,0:1]
-    return np.unique(data).size
+def get_unique_teams_set():
+    teams = []
+    for i in read_ipl_data_csv(path, dtype='|S100'):
+        if not i[3] in teams:
+            teams.append(i[3])
+        if not i[4] in teams:
+            teams.append(i[4])
+    return set(teams)
+
+get_unique_teams_set()
+
 
 
