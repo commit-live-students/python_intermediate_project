7ccc48c92f8f9509a020d0a4244ecb3fb7bb8bc6
paragDC <paragchache06@gmail.com>
2018-12-21 12:58:14 +0000

    Done

--- q05_read_csv_data/build.py
+++ q05_read_csv_data/build.py
@@ -1,4 +1,34 @@
+# %load q05_read_csv_data/build.py
 # Default imports
 import numpy as np
 
 # Enter code here
+from greyatomlib.python_intermediate.q05_read_csv_data.build import read_ipl_data_csv
+path = 'data/ipl_matches_small.csv'
+
+import numpy as np
+import csv
+
+def get_unique_matches_count():
+    
+    no_of_matches_played = 0
+    with open(path,'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        data = [row for row in csvreader] 
+        np_ipl_data = np.asarray(data)
+
+    headers = tuple(np_ipl_data[0])
+    
+    #slicing main ndarray into required ndarray
+    match_codes = np_ipl_data[1:,[headers.index('match_code')]]
+    
+    #reshaping ndarray from 2d to 1d to make a list
+    match_codes_array = match_codes.reshape(np.product(match_codes.shape))
+    
+    #converting 1d array to a list and then removing duplicates to get unique values
+    match_codes_set = set(match_codes_array.tolist())
+    
+    #length of the set will be the total number of matches played
+    no_of_matches_played = len(match_codes_set)
+    return no_of_matches_played
+
